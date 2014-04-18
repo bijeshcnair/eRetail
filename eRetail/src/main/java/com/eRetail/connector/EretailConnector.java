29b3c43930fcfb3e57f0926ffe1267ffc73852a6
biju <bijeshcnair@gmail.com>
2014-04-18 05:53:36 +0530

    Added class comments for eRetail Conneactor

--- eRetail/src/main/java/com/eRetail/connector/EretailConnector.java
+++ eRetail/src/main/java/com/eRetail/connector/EretailConnector.java
@@ -6,7 +6,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * Servlet connector class for eRetail
+ * @author biju
+ *
+ */
 public class EretailConnector extends HttpServlet {
 
 
